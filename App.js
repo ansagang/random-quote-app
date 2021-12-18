3087f6b800bec4b20b94d17b8228b9bfe285089f
ansagang <ansagaang@gmail.com>
2021-12-19 03:33:15 +0600

    template

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Quote app</Text>
+      <Text>Random quote app</Text>
       <StatusBar style="auto" />
     </View>
   );
